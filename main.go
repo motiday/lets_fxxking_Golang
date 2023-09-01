1a1eabe0cb5b669256feb1ad6c4d5d0c7f84f6c7
Tomoya Oiso <34730701+motiday@users.noreply.github.com>
2023-09-01 12:03:42 +0900

    hello golang

---
+++ main.go
@@ -0,0 +1,10 @@
+// main.go
+package main // パッケージ宣言
+
+import "fmt" // fmtパッケージのインポート
+
+func main() { // メイン関数の定義
+    fmt.Println("Hello, world!") // Println関数の呼び出し
+
+	fmt.Println("日本語もいけんの買って")
+}
